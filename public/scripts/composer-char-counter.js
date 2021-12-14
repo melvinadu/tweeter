ede0187680da14bb4cef15f7a0949f84eb7dac5e
Melvin Adu <contactmelvinadu@gmail.com>
2021-12-14 23:43:49 +0000

    add section for list of tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,14 +2,16 @@ $(document).ready(function() {
   // --- our code goes here ---
 
   $('#tweet-text').on('input', function() {
-    
+    //declaring variable we will keep count of text box characters with
     let currentCount = 140;
 
     //traverse up the DOM tree and find the node with class of counter
     const counter = $(this).closest('.new-tweet').find('.counter');
+
     //obtaining value of "this" (#tweet-text)
     let value = $(this).val();
-    
+
+    //keeping count of number of characters in the textbox
     currentCount -= value.length;
 
     //changing value of counter to currentCount variable value
