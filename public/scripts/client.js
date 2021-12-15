84a478ba2c3c1daafa7b95a08bf33b8d02b8533d
Melvin Adu <contactmelvinadu@gmail.com>
2021-12-15 21:37:47 +0000

    submit new tweet data to server and fetch tweets with jquery and ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,23 +6,47 @@
 
 $(document).ready(function() {
 
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
   const createTweetElement = (tweet) => {
+    const $tweet = $(`<article>`).addClass('tweet');
 
-  const $tweet = $(`<article>`).addClass('tweet');
-
-  let html = `<header class="tweet-header">
-      <div>${tweet.user.name}</div>
-      <div class="handle">${tweet.user.handle}</div>
-    </header>
-    <p>${tweet.content.text}</p>
-    <footer class="tweet-footer">
-      <div>${timeago.format(tweet.created_at)}</div>
-      <div class="icons">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>`;
+    let html = `<header class="tweet-header">
+        <div>${tweet.user.name}</div>
+        <div class="handle">${tweet.user.handle}</div>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer class="tweet-footer">
+        <div>${timeago.format(tweet.created_at)}</div>
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>`;
 
     let tweetElement = $tweet.append(html);
 
@@ -32,43 +56,43 @@ $(document).ready(function() {
 const renderTweets = function(tweets) {
   //empty container
   $('#tweets').empty();
-
   // loops through tweets
   for (let tweet of tweets) {
   // calls createTweetElement for each tweet
   const $tweet = createTweetElement(tweet);
   const $tweetsContainer = $('#tweets');
-  // takes return value and appends it to the tweets container
+  // takes return value and prepends it to the tweets container
   $tweetsContainer.prepend($tweet);
   }
 }
 
+// renderTweets(tweetData);
 
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+const $form = $('#new-tweet-form');
 
-renderTweets(tweetData);
+$form.submit(function(event) {
+  event.preventDefault();
+
+  const serializedData = $(this).serialize();
+  
+  $.post('/tweets', serializedData, (response) => {
+  })
 
 });
+
+const loadTweets = function() {
+
+    console.log('Performing ajax call...');
+
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (response) {
+      renderTweets(response);
+      console.log('Success: ', response);
+    });
+  
+};
+
+loadTweets();
+
+});
+
